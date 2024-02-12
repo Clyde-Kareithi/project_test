b8eab9d37d59d38360760cb9f145ad365ff51452
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2024-02-12 13:55:55 +0300

    remove commented `sell_car()`

--- commerce/views.py
+++ commerce/views.py
@@ -4,16 +4,6 @@ from .models import CarCommerce
 from cars.models import CarModel, BrandModel
 from .forms import RecommendationForm, YourCarForm
 
-# Create your views here.
-# @login_required
-# def sell_car(request, car_name, description, brand, quantity,  price, image):
-#     existing_brand = BrandModel.objects.get_or_create(brand_name=brand)
-#     car = CarModel.objects.create(car_name=car_name, description=description, brand=existing_brand, quantity=quantity, price=price, image=image)
-#     current_user = request.user
-#     car_for_sale = CarCommerce.objects.create(car=car, user=current_user)
-#     car_for_sale.save()
-
-#     return car_for_sale
 
 def buy_car(request):
     # Get distinct body styles and brands
