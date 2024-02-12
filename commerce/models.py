38f4b0d9f27544d451dd55269a73ab1ce662a484
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2024-02-12 13:46:11 +0300

    refactor commerce model

--- commerce/models.py
+++ commerce/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from cars.models import CarModel
 
-# Create your models here.
 class CarCommerce(models.Model):
     id = models.IntegerField
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -10,3 +9,6 @@ class CarCommerce(models.Model):
 
     def __str__(self):
         return self.user.username + ' ' + self.car.car_name
+    
+    
+    
